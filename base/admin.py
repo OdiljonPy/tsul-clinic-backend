cd2da562fc65df923c2eb0d2e172e949b8763b89
sattorov <sattorov.ahu@gmail.com>
2024-09-07 22:26:26 +0500

    Models added admin panel

--- base/admin.py
+++ base/admin.py
@@ -1,3 +1,96 @@
 from django.contrib import admin
+from .models import (
+    Banner, News, Time, Statistics, CustomerOpinion,
+    FAQ, AboutUs, Info, OfficeAddress, ServicesCategory,
+    Services, AdditionalLinks
+)
 
-# Register your models here.
+
+@admin.register(Banner)
+class BannerAdmin(admin.ModelAdmin):
+    list_display = ('id', 'text')
+    list_display_links = ('id', 'text')
+    search_fields = ('id', 'text')
+
+
+@admin.register(News)
+class NewsAdmin(admin.ModelAdmin):
+    list_display = ('id', 'title', 'short_description', 'views_count', 'is_published')
+    list_display_links = ('id', 'title')
+    search_fields = ('id', 'title', 'short_description')
+    list_filter = ('views_count', 'is_published')
+
+
+@admin.register(Time)
+class TimeAdmin(admin.ModelAdmin):
+    list_display = ('id', 'full_name', 'position')
+    list_display_links = ('id', 'full_name')
+    search_fields = ('id', 'full_name', 'position')
+    list_filter = ('position',)
+
+
+@admin.register(Statistics)
+class StatisticsAdmin(admin.ModelAdmin):
+    list_display = ('id', 'customers_number', 'services_number', 'service_indicator')
+    list_display_links = ('id', 'customers_number', 'services_number', 'service_indicator')
+    search_fields = ('id', 'customers_number', 'services_number', 'service_indicator')
+    list_filter = ('customers_number', 'services_number', 'service_indicator')
+
+
+@admin.register(CustomerOpinion)
+class CustomerOpinionAdmin(admin.ModelAdmin):
+    list_display = ('id', 'company_name', 'full_name', 'position')
+    list_display_links = ('id', 'company_name', 'full_name')
+    search_fields = ('id', 'company_name', 'full_name', 'position')
+    list_filter = ('company_name', 'position')
+
+
+@admin.register(FAQ)
+class FAQAdmin(admin.ModelAdmin):
+    list_display = ('id', 'question', 'is_published')
+    list_display_links = ('id', 'question')
+    search_fields = ('id', 'question')
+
+
+@admin.register(AboutUs)
+class AboutUsAdmin(admin.ModelAdmin):
+    list_display = ('id', 'our_goal')
+    list_display_links = ('id', 'our_goal')
+    search_fields = ('id', 'our_goal')
+
+
+class OfficeAddressAdmin(admin.TabularInline):
+    model = OfficeAddress
+    fields = ('id', 'name')
+    extra = 1
+
+
+@admin.register(Info)
+class InfoAdmin(admin.ModelAdmin):
+    list_display = ('id', 'phone_number', 'telegram', 'linkedin')
+    list_display_links = ('id', 'phone_number', 'telegram')
+    search_fields = ('id', 'phone_number', 'telegram', 'linkedin')
+    inlines = (OfficeAddressAdmin,)
+
+
+@admin.register(ServicesCategory)
+class ServicesCategoryAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name')
+    list_display_links = ('id', 'name')
+    search_fields = ('id', 'name')
+
+
+@admin.register(Services)
+class ServicesAdmin(admin.ModelAdmin):
+    list_display = ('id', 'category', 'name')
+    list_display_links = ('id', 'category', 'name')
+    search_fields = ('id', 'category', 'name')
+    list_filter = ('category',)
+
+
+@admin.register(AdditionalLinks)
+class AdditionalLinksAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name', 'is_published')
+    list_display_links = ('id', 'name')
+    search_fields = ('id', 'name')
+    list_filter = ('is_published',)
