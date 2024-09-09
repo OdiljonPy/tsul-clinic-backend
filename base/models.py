ca9fc2e26ca2cad49fc7b58daeb3fc2ca2a82ac7
suxrob <suxrobergashev2102@fgamail.com>
2024-09-09 15:19:34 +0000

    migrations added CustomerOpinion image

--- base/models.py
+++ base/models.py
@@ -67,6 +67,7 @@ class CustomerOpinion(base_models.BaseModel):
     position = models.CharField(max_length=50, verbose_name="Позиция")
     full_name = models.CharField(max_length=150, verbose_name="Полное имя")
     opinion = models.CharField(max_length=150, verbose_name="Мнение")
+    image = models.ImageField(null=True, blank=True, upload_to='customer_opinion/')
 
     def __str__(self):
         return self.company_name
