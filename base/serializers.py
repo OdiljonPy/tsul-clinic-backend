533ec9d5ca376a523a1251ba7b45aef0ac4854b6
sattorov <sattorov.ahu@gmail.com>
2024-09-09 16:58:40 +0500

    bug fixed

--- base/serializers.py
+++ base/serializers.py
@@ -218,6 +218,6 @@ class PaginatorSerializer(serializers.Serializer):
     def validate(self, attrs):
         page = attrs.get('page')
         page_size = attrs.get('page_size')
-        if page_size > 0 or page > 0:
+        if page_size < 0 or page < 0:
             raise CustomApiException(ErrorCodes.VALIDATION_FAILED, message='Page or page size is invalid')
         return attrs
