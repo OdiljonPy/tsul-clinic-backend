5a6de51a877addba1e4ffb92ba4ff75455956df3
suxrob <suxrobergashev2102@fgamail.com>
2024-09-10 14:24:10 +0000

    change urls

--- serviceses/urls.py
+++ serviceses/urls.py
@@ -4,6 +4,6 @@ from .views import DocumentCategoryViewSet, DocumentOrderViewSet, ContactsViewSe
 urlpatterns = [
     path('category/', DocumentCategoryViewSet.as_view({'get': 'list'}), name='category'),
     path('create/document/', DocumentOrderViewSet.as_view({'post': 'create'}), name='create'),
-    path('create/meeting/', DocumentOrderViewSet.as_view({'post': 'create'}), name='create_meeting'),
+    path('create/meeting/', DocumentOrderViewSet.as_view({'post': 'create_meeting'}), name='create_meeting'),
     path('create/contacts/', ContactsViewSet.as_view({'post': 'create'}), name='create_contacts'),
 ]
